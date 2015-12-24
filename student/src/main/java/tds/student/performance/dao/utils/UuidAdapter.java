bbd620683cfa543301afe5117e77353ba9eaae37
Jeff Johnson <jjohnson@fairwaytech.com>
2015-12-23 18:16:36 -0800

    Added unit tests, minor refactoring.

--- student/src/main/java/tds/student/performance/dao/utils/UuidAdapter.java
+++ student/src/main/java/tds/student/performance/dao/utils/UuidAdapter.java
@@ -7,11 +7,6 @@ import java.util.UUID;
  * Created by jjohnson on 12/23/15.
  */
 public class UuidAdapter {
-    public static byte[] getBytesFromUuidString(String uuidString) {
-        return getBytesFromUUID(UUID.fromString(uuidString));
-    }
-
-    // TODO:  Is this method even required?  Will we ever have an actual UUID that we need converted to a byte array?
     public static byte[] getBytesFromUUID(UUID uuid) {
         ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
         bb.putLong(uuid.getMostSignificantBits());
@@ -20,7 +15,7 @@ public class UuidAdapter {
         return bb.array();
     }
 
-    public static UUID getUuidFromBytes(byte[] bytes) {
+    public static UUID getUUIDFromBytes(byte[] bytes) {
         ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
         Long high = byteBuffer.getLong();
         Long low = byteBuffer.getLong();
