877151de63cca375e99b89fae9a7a5d1d682f685
Jeff Treuting <jtreuting@fairwaytech.com>
2016-01-02 22:55:51 -0800

    Initial optimizations for TestShell.aspx - Took ItemBankRepository and moved it into our own ItemBankService (since it has logic around multiple SQL calls) and enabled caching on some methods for getting a Tests properties and Test Accommodations - Both of these methods only rely on the testKey and joins on configuration tables, so are perfect for caching - Updated calls into the old methods to point to the new methods (except in the old tests)

--- student/src/main/java/tds/student/performance/dao/ItemBankDao.java
+++ student/src/main/java/tds/student/performance/dao/ItemBankDao.java
@@ -1,10 +1,11 @@
 package tds.student.performance.dao;
 
 import tds.student.performance.domain.SetOfAdminSubject;
+import tds.student.sql.data.TestGrade;
 
-/**
- * Created by jjohnson on 12/25/15.
- */
+import java.util.List;
+
 public interface ItemBankDao {
     SetOfAdminSubject get(String adminSubject);
+    List<TestGrade> getTestGrades(String clientName, String testKey, Integer sessionType);
 }
--- student/src/main/java/tds/student/performance/dao/impl/ItemBankDaoImpl.java
+++ student/src/main/java/tds/student/performance/dao/impl/ItemBankDaoImpl.java
@@ -1,5 +1,6 @@
 package tds.student.performance.dao.impl;
 
+import AIR.Common.DB.SqlParametersMaps;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +10,12 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 import tds.student.performance.dao.ItemBankDao;
 import tds.student.performance.domain.SetOfAdminSubject;
+import tds.student.sql.data.TestGrade;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -56,5 +60,31 @@ public class ItemBankDaoImpl implements ItemBankDao {
             logger.warn(String.format("%s did not return results for adminSubject = %s", SQL, adminSubject));
             return null;
         }
+    }
+
+    @Override
+    public List<TestGrade> getTestGrades(String clientName, String testKey, Integer sessionType) {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put ("testKey", testKey);
+        parameters.put ("clientName", clientName);
+        parameters.put ("sessionType", sessionType);
+
+        final String SQL = "select distinct grade "
+                + " from itembank.tblsetofadminsubjects S, configs.client_testmode M, configs.client_testgrades G, configs.client_testwindow W, configs.client_testproperties P "
+                + " where M.clientname = :clientName and (:testKey is null or M.testkey = :testKey) and M.testkey = S._Key and (M.sessionType = -1 or M.sessionType = :sessionType) "
+                + "    and M.clientname = G.clientname and M.TestID = G.TestID  and W.clientname = :clientName and W.TestID = M.testID and P.clientname = :clientName and P.TestID = M.testID "
+                + "    and P.IsSelectable = 1 and now() between W.startDate and W.endDate order by grade";
+
+        List<Map<String, Object>> list = namedParameterJdbcTemplate.queryForList(
+                SQL,
+                parameters);
+
+        List<TestGrade> results = new ArrayList<>();
+
+        for (Map<String, Object> row : list) {
+            results.add(new TestGrade((String)row.get("grade")));
+        }
+
+        return results;
     }
 }
