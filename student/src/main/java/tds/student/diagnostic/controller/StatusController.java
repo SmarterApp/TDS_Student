09bd15440ca3087e4f85135bf9f40373c475c8cb
Jeff Treuting <jtreuting@fairwaytech.com>
2016-04-03 14:14:47 -0700

    Refactored diagnostic API to put common classes into tdsdll_release in order for Proctor to use them as well

--- student/src/main/java/tds/student/diagnostic/controller/StatusController.java
+++ student/src/main/java/tds/student/diagnostic/controller/StatusController.java
@@ -13,228 +13,101 @@
 
 package tds.student.diagnostic.controller;
 
-import org.joda.time.DateTime;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import tds.student.diagnostic.dao.ReadTestDao;
-import tds.student.diagnostic.dao.WriteTestDao;
-import tds.student.diagnostic.domain.Level;
-import tds.student.diagnostic.domain.Rating;
-import tds.student.diagnostic.domain.Status;
-import tds.student.diagnostic.domain.Summary;
-import tds.student.diagnostic.services.DiagnosticConfigurationService;
-import tds.student.diagnostic.services.DiagnosticDependencyService;
-import tds.student.diagnostic.services.DiagnosticSystemService;
-import tds.student.diagnostic.services.impl.DiagnosticDatabaseServiceImpl;
+import tds.dll.common.diagnostic.controller.AbstractStatusController;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 @Controller
-public class StatusController {
+public class StatusController extends AbstractStatusController {
 
-    //private static final Logger logger = LoggerFactory.getLogger(StatusController.class);
+    public StatusController() {
+        super("Student", Arrays.asList(
+                "component.name",
 
-    @Autowired
-    ReadTestDao readTestDao;
+                "datasource.acquireRetryAttempts",
+                "datasource.minPoolSize",
+                "datasource.testConnectionOnCheckout",
+                "datasource.maxPoolSize",
+                "datasource.numHelperThreads",
+                "datasource.maxStatements",
+                "datasource.idleConnectionTestPeriod",
+                "datasource.testConnectionOnCheckin",
+                "datasource.url", // TODO: is this safe?
+                "datasource.checkoutTimeout",
+                "datasource.maxStatementsPerConnection",
+                "datasource.driverClassName",
+                "datasource.acquireIncrement",
+                "datasource.maxConnectionAge",
 
-    @Autowired
-    WriteTestDao writeTestDao;
+                "dbLockRetrySleepInterval",
+                "dbLockRetryAttemptMax",
 
-    @Autowired
-    DiagnosticSystemService diagnosticSystemService;
+                "iris.ContentPath",
+                "itemscoring.qti.sympyTimeoutMillis",
+                "itemscoring.qti.sympyServiceUrl",
+                "itemscoring.qti.sympyMaxTries",
+                "itemScoring.callbackUrl",
 
-    @Autowired
-    DiagnosticConfigurationService diagnosticConfigurationService;
+                "logLatencyInterval",
+                "logLatencyMaxTime",
 
-    @Autowired
-    DiagnosticDatabaseServiceImpl diagnosticDatabaseService;
+                "mnaNodeName",
+                "mnaServerName",
+                "mna.mnaUrl",
+                "mna.logger.level",
 
-    @Autowired
-    private DiagnosticDependencyService diagnosticDependencyService;
+                "opportunity.isScoredByTDS",
 
+                "performance.logMaxTestOpportunities.enabled",
+                "performance.logLatency.enabled",
+                "performance.datasource.maxPoolSize",
+                "performance.datasource.minPoolSize",
 
-    @RequestMapping(value = "/status", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public Status getStatus(@RequestParam(value = "level", required = false, defaultValue = "0") Integer level,
-                            @RequestParam(value = "single", required = false, defaultValue = "0") Boolean single) {
+                "permission.uri",
+                "permission.security.profile",
 
-        return processLevel(level, single);
+                "proctor.AppName",
+                "proctor.Appkey",
+                "proctor.StateCode",
+                "proctor.TestRegistrationApplicationUrl",
+                "proctor.ItembankDBName",
+                "proctor.IsCheckinSite",
+                "proctor.TDSSessionDBName",
+                "proctor.ClientQueryString",
+                "proctor.ClientName",
+                "proctor.TDSArchiveDBName",
+                "proctor.security.dir",
+                "proctor.webapp.saml.metadata.filename",
+                "proctor.SessionType",
+                "proctor.security.idp", // TODO: is this safe?
+                "proctor.Debug.AllowFTP",
+                "proctor.DBJndiName",
+                "proctor.RecordSystemClient",
+                "proctor.TDSConfigsDBName",
+                "proctor.SqlCommandTimeout",
+
+                "student.AppName",
+                "student.ItembankDBName",
+                "student.testScoring.logDebug",
+                "student.TDSConfigsDBName",
+                "student.RecordSystemClient",
+                "student.Debug.AllowFTP",
+                "student.TDSReportsRootDirectory",
+                "student.StateCode",
+                "student.SqlCommandTimeout",
+                "student.StudentMaxOpps", // TODO: is this used anywhere?
+                "student.TDSSessionDBName",
+                "student.ClientCookie",
+                "student.IsCheckinSite",
+                "student.ClientQueryString",
+                "student.TDSArchiveDBName",
+                "student.Appkey",
+                "student.SessionType",
+                "student.DBDialect",
+                "student.testScoring.logError",
+                "student.ClientName",
+                "student.TestRegistrationApplicationUrl"
+        ));
     }
-
-    @RequestMapping(value = "/status/summary", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public Summary getSummary(@RequestParam(value = "level", required = false, defaultValue = "0") Integer level,
-                              @RequestParam(value = "single", required = false, defaultValue = "0") Boolean single) {
-
-        return new Summary(processLevel(level, single));
-    }
-
-    private Status processLevel(Integer level, Boolean single) {
-        switch (level) {
-            case 1:
-                return levelLocalSystem();
-            case 2:
-                return levelConfig(single);
-            case 3:
-                return levelReadData(single);
-            case 4:
-                return levelWriteData(single);
-            case 5:
-                return levelDependencies(single);
-            case 0:
-            default:
-                return new Status("Student", Level.LEVEL_0, Rating.IDEAL, new DateTime());
-        }
-    }
-
-    // Local System Level 1
-    private Status levelLocalSystem() {
-
-        Status status = new Status("Student", Level.LEVEL_1, new DateTime());
-        status.setLocalSystem(diagnosticSystemService.getSystem());
-        return status;
-    }
-
-    // Config Level 2
-    private Status levelConfig(Boolean single) {
-
-        Status status = new Status("Student", Level.LEVEL_2, new DateTime());
-        if ( !single ) {
-            status.setLocalSystem(diagnosticSystemService.getSystem());
-        }
-        status.setConfiguration(diagnosticConfigurationService.getConfiguration(configPropertyWhitelist));
-        return status;
-    }
-
-    // Read Level 3
-    private Status levelReadData(Boolean single) {
-
-        Status status = new Status("Student", Level.LEVEL_3, new DateTime());
-        if ( !single ) {
-            status.setLocalSystem(diagnosticSystemService.getSystem());
-            status.setConfiguration(diagnosticConfigurationService.getConfiguration(configPropertyWhitelist));
-        }
-        status.setDatabase(diagnosticDatabaseService.readLevelTest());
-        return status;
-    }
-
-    // Write Level 4
-    private Status levelWriteData(Boolean single) {
-
-        Status status = new Status("Student", Level.LEVEL_4, new DateTime());
-        if ( !single ) {
-            status.setLocalSystem(diagnosticSystemService.getSystem());
-            status.setConfiguration(diagnosticConfigurationService.getConfiguration(configPropertyWhitelist));
-        }
-        status.setDatabase(diagnosticDatabaseService.writeLevelTest());
-        return status;
-    }
-
-    // Dependencies Level 5
-    private Status levelDependencies(Boolean single) {
-
-        Status status = new Status("Student", Level.LEVEL_5, new DateTime());
-        if ( !single ) {
-            status.setLocalSystem(diagnosticSystemService.getSystem());
-            status.setConfiguration(diagnosticConfigurationService.getConfiguration(configPropertyWhitelist));
-            status.setDatabase(diagnosticDatabaseService.writeLevelTest());
-        }
-        status.setProviders(diagnosticDependencyService.getProviders());
-        return status;
-    }
-
-    final private List<String> configPropertyWhitelist = Arrays.asList(
-            "component.name",
-
-            "datasource.acquireRetryAttempts",
-            "datasource.minPoolSize",
-            "datasource.testConnectionOnCheckout",
-            "datasource.maxPoolSize",
-            "datasource.numHelperThreads",
-            "datasource.maxStatements",
-            "datasource.idleConnectionTestPeriod",
-            "datasource.testConnectionOnCheckin",
-            "datasource.url", // TODO: is this safe?
-            "datasource.checkoutTimeout",
-            "datasource.maxStatementsPerConnection",
-            "datasource.driverClassName",
-            "datasource.acquireIncrement",
-            "datasource.maxConnectionAge",
-
-            "dbLockRetrySleepInterval",
-            "dbLockRetryAttemptMax",
-
-            "iris.ContentPath",
-            "itemscoring.qti.sympyTimeoutMillis",
-            "itemscoring.qti.sympyServiceUrl",
-            "itemscoring.qti.sympyMaxTries",
-            "itemScoring.callbackUrl",
-
-            "logLatencyInterval",
-            "logLatencyMaxTime",
-
-            "mnaNodeName",
-            "mnaServerName",
-            "mna.mnaUrl",
-            "mna.logger.level",
-
-            "opportunity.isScoredByTDS",
-
-            "performance.logMaxTestOpportunities.enabled",
-            "performance.logLatency.enabled",
-            "performance.datasource.maxPoolSize",
-            "performance.datasource.minPoolSize",
-
-            "permission.uri",
-            "permission.security.profile",
-
-            "proctor.AppName",
-            "proctor.Appkey",
-            "proctor.StateCode",
-            "proctor.TestRegistrationApplicationUrl",
-            "proctor.ItembankDBName",
-            "proctor.IsCheckinSite",
-            "proctor.TDSSessionDBName",
-            "proctor.ClientQueryString",
-            "proctor.ClientName",
-            "proctor.TDSArchiveDBName",
-            "proctor.security.dir",
-            "proctor.webapp.saml.metadata.filename",
-            "proctor.SessionType",
-            "proctor.security.idp", // TODO: is this safe?
-            "proctor.Debug.AllowFTP",
-            "proctor.DBJndiName",
-            "proctor.RecordSystemClient",
-            "proctor.TDSConfigsDBName",
-            "proctor.SqlCommandTimeout",
-
-            "student.AppName",
-            "student.ItembankDBName",
-            "student.testScoring.logDebug",
-            "student.TDSConfigsDBName",
-            "student.RecordSystemClient",
-            "student.Debug.AllowFTP",
-            "student.TDSReportsRootDirectory",
-            "student.StateCode",
-            "student.SqlCommandTimeout",
-            "student.StudentMaxOpps", // TODO: is this used anywhere?
-            "student.TDSSessionDBName",
-            "student.ClientCookie",
-            "student.IsCheckinSite",
-            "student.ClientQueryString",
-            "student.TDSArchiveDBName",
-            "student.Appkey",
-            "student.SessionType",
-            "student.DBDialect",
-            "student.testScoring.logError",
-            "student.ClientName",
-            "student.TestRegistrationApplicationUrl"
-    );
-
 }
