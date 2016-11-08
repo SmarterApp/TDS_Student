bd79764ba79dfb5db96f6811d109afe342dda4b9
shajib khan <shajibcse067@gmail.com>
2016-11-08 17:18:48 -0500

    Updated css from Release 9.0

---
+++ student/src/main/webapp/tools/PeriodicTable.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TDS.Student.Web.UI.Tools
+{
+    public partial class PeriodicTable : StudentPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
