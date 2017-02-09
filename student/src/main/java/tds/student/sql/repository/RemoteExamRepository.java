3659bd0006dd1a90cf0c31fb2e9e614d628bcbb8
Ernie Munoz <emunoz@fairwaytech.com>
2017-02-08 17:53:27 -0800

    Fixing compilation error

--- student/src/main/java/tds/student/sql/repository/RemoteExamRepository.java
+++ student/src/main/java/tds/student/sql/repository/RemoteExamRepository.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.HttpClientErrorException;
