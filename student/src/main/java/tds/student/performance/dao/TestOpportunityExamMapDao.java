cb4c286d8159477f1744679b01c86b7bc0cba975
Jeff Treuting <jtreuting@fairwaytech.com>
2017-02-23 11:12:22 -0800

    Added java docs

--- student/src/main/java/tds/student/performance/dao/TestOpportunityExamMapDao.java
+++ student/src/main/java/tds/student/performance/dao/TestOpportunityExamMapDao.java
@@ -2,6 +2,16 @@ package tds.student.performance.dao;
 
 import java.util.UUID;
 
+/**
+ * Used to store mapping from legacy test opportunity IDs to the new exam IDs.  This is used when in the mode of calling
+ * both the legacy services as well as the new services.  Eventually this will be removed.
+ */
 public interface TestOpportunityExamMapDao {
+
+    /**
+     * Insert a record mapping the test opportunity ID and exam ID
+     * @param testOpportunityId test opportunity ID from session.testopportunity table
+     * @param examId exam ID used in the new exam.exam table
+     */
     void insert(final UUID testOpportunityId, final UUID examId);
 }
