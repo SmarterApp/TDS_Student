4bbf69d44f6f3e38040edbb0ddd5982622a0950a
Will <wtritch@users.noreply.github.com>
2017-05-01 13:44:30 -0700

    Add remote item scoring service implementation. (#63)
    * Add remote item scoring service implementation.
    
    * Update to remove unnecessary matchers in tests.
    
    * Bump item-renderer version
    
    * Add support for handling the end of a test, with no more item groups to be created.

---
+++ student/src/main/java/tds/student/services/remote/RemoteItemScoringService.java
@@ -0,0 +1,108 @@
+package tds.student.services.remote;
+
+import TDS.Shared.Exceptions.ReturnStatusException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import tds.itemrenderer.data.IITSDocument;
+import tds.itemscoringengine.ItemScore;
+import tds.student.services.abstractions.IItemScoringService;
+import tds.student.sql.data.IItemResponseScorable;
+import tds.student.sql.data.ItemResponseUpdate;
+import tds.student.sql.data.ItemResponseUpdateStatus;
+import tds.student.sql.data.OpportunityInstance;
+import tds.student.sql.repository.remote.ItemScoringRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * This IItemScoringService implementation forwards scoring requests to the ItemScoringRepository.
+ */
+@Service("integrationItemScoringService")
+@Scope("prototype")
+public class RemoteItemScoringService implements IItemScoringService {
+
+    private final IItemScoringService legacyItemScoringService;
+    private final boolean remoteExamCallsEnabled;
+    private final boolean legacyCallsEnabled;
+    private final ItemScoringRepository repository;
+
+
+    @Autowired
+    public RemoteItemScoringService(
+        @Qualifier("legacyItemScoringService") final IItemScoringService legacyItemScoringService,
+        @Value("${tds.exam.remote.enabled}") final Boolean remoteExamCallsEnabled,
+        @Value("${tds.exam.legacy.enabled}") final Boolean legacyCallsEnabled,
+        final ItemScoringRepository repository) {
+
+        if (!remoteExamCallsEnabled && !legacyCallsEnabled) {
+            throw new IllegalStateException("Remote and legacy calls are both disabled.  Please check progman configuration");
+        }
+
+        this.legacyItemScoringService = legacyItemScoringService;
+        this.remoteExamCallsEnabled = remoteExamCallsEnabled;
+        this.legacyCallsEnabled = legacyCallsEnabled;
+        this.repository = repository;
+    }
+
+    @Override
+    public List<ItemResponseUpdateStatus> updateResponses(final OpportunityInstance oppInstance,
+                                                          final List<ItemResponseUpdate> responsesUpdated,
+                                                          final Float pageDuration) throws ReturnStatusException {
+        List<ItemResponseUpdateStatus> responseUpdateStatuses = new ArrayList<>();
+
+        if (legacyCallsEnabled) {
+            responseUpdateStatuses = legacyItemScoringService.updateResponses(oppInstance, responsesUpdated, pageDuration);
+        }
+
+        if (!remoteExamCallsEnabled) {
+            return responseUpdateStatuses;
+        }
+
+        responseUpdateStatuses = repository.updateResponses(
+            oppInstance.getExamId(),
+            oppInstance.getSessionKey(),
+            oppInstance.getExamBrowserKey(),
+            oppInstance.getExamClientName(),
+            pageDuration,
+            responsesUpdated);
+
+        return responseUpdateStatuses;
+    }
+
+    @Override
+    public ItemScore checkScoreability(final IItemResponseScorable responseScorable, final IITSDocument itsDoc) throws ReturnStatusException {
+        if (legacyCallsEnabled) {
+            return legacyItemScoringService.checkScoreability(responseScorable, itsDoc);
+        }
+
+        //This method is only called via a legacy service self-reference and via the unused TimerScoringService
+        throw new UnsupportedOperationException("No remote implementation of checkScoreability");
+    }
+
+    @Override
+    public boolean updateItemScore(final UUID oppKey, final IItemResponseScorable response, final ItemScore score) throws ReturnStatusException {
+        if (legacyCallsEnabled) {
+            return legacyItemScoringService.updateItemScore(oppKey, response, score);
+        }
+
+        //This method is only called via a legacy service self-reference and
+        // via the unused TimerScoringService and ItemScoringCallbackHandler
+        throw new UnsupportedOperationException("No remote implementation of updateItemScore");
+    }
+
+    @Override
+    public ItemScore scoreItem(final UUID oppKey, final IItemResponseScorable responseScorable, final IITSDocument itsDoc) throws ReturnStatusException {
+        if (legacyCallsEnabled) {
+            return legacyItemScoringService.scoreItem(oppKey, responseScorable, itsDoc);
+        }
+
+        //This method is only called via a legacy service self-reference and
+        // via the unused TimerScoringService and ItemScoringCallbackHandler
+        throw new UnsupportedOperationException("No remote implementation of scoreItem");
+    }
+}
---
+++ student/src/main/java/tds/student/sql/repository/remote/impl/RemoteItemScoringRepository.java
@@ -0,0 +1,76 @@
+package tds.student.sql.repository.remote.impl;
+
+import TDS.Shared.Exceptions.ReturnStatusException;
+import com.rabbitmq.client.AMQP;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import tds.student.sql.data.ItemResponseUpdate;
+import tds.student.sql.data.ItemResponseUpdateStatus;
+import tds.student.sql.repository.remote.ItemScoringRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * This implementation of an ItemScoringRepository forwards item responses
+ * to a remote exam service.
+ */
+@Repository
+public class RemoteItemScoringRepository implements ItemScoringRepository {
+
+    private final RestTemplate restTemplate;
+    private final String examUrl;
+
+    @Autowired
+    public RemoteItemScoringRepository(@Qualifier("integrationRestTemplate") final RestTemplate restTemplate,
+                                       @Value("${tds.exam.remote.url}") final String examUrl) {
+        this.restTemplate = restTemplate;
+        this.examUrl = examUrl;
+    }
+
+    @Override
+    public List<ItemResponseUpdateStatus> updateResponses(final UUID examId,
+                                                          final UUID sessionId,
+                                                          final UUID browserId,
+                                                          final String clientName,
+                                                          final Float pageDuration,
+                                                          final List<ItemResponseUpdate> responseUpdates) throws ReturnStatusException {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        final HttpEntity<List<ItemResponseUpdate>> requestHttpEntity = new HttpEntity<>(responseUpdates, headers);
+
+        final UriComponentsBuilder builder = UriComponentsBuilder
+            .fromHttpUrl(String.format("%s/%s/scores/responses", examUrl, examId))
+            .queryParam("sessionId", sessionId)
+            .queryParam("browserId", browserId)
+            .queryParam("clientName", clientName)
+            .queryParam("pageDuration", pageDuration);
+
+        try {
+            return restTemplate.exchange(
+                builder.build().toUri(),
+                HttpMethod.PUT,
+                requestHttpEntity,
+                new ParameterizedTypeReference<List<ItemResponseUpdateStatus>>() {
+                }).getBody();
+        } catch (final HttpStatusCodeException e) {
+            final ReturnStatusException statusException = new ReturnStatusException("Failed to update response: " + e.getResponseBodyAsString());
+            statusException.getReturnStatus().setHttpStatusCode(500);
+            throw statusException;
+        } catch (final RestClientException rce) {
+            throw new ReturnStatusException(rce);
+        }
+    }
+}
