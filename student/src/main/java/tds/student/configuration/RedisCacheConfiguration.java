571cd2a1d86e450e0359e9e8158870da8ee4c62d
Will <wtritch@users.noreply.github.com>
2017-06-06 07:13:00 -0700

    Load test tomcat config (#86)
    * Override Tomcat server.xml in docker image with additional configurations.
    
    * Enable legacy hikari connection pool leak detection.
    Enable configurable connection timeout.
    
    * Add redis caching configuration
    
    * Update concurrent request limiter with logging.
    
    * Update redis configuration to handle a single redis host connection.

--- student/src/main/java/tds/student/configuration/RedisCacheConfiguration.java
+++ student/src/main/java/tds/student/configuration/RedisCacheConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.util.StringUtils;
@@ -17,6 +18,8 @@ import tds.dll.common.performance.caching.impl.RedisJsonSerializer;
 
 import java.util.Set;
 
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 /**
  * This configuration initializes a Redis Sentinel-backed cache manager if the "redisCaching" profile is active.
  */
@@ -25,10 +28,20 @@ import java.util.Set;
 public class RedisCacheConfiguration {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory(@Value("${spring.redis.sentinel.master}") final String masterName,
-                                                         @Value("${spring.redis.sentinel.nodes}") final String sentinelCsv) {
+    public RedisConnectionFactory redisConnectionFactory(@Value("${spring.redis.sentinel.master:}") final String masterName,
+                                                         @Value("${spring.redis.sentinel.nodes:}") final String sentinelCsv,
+                                                         @Value("${spring.redis.host:}") final String redisHost,
+                                                         @Value("${spring.redis.port:6379}") final int redisPort) {
+
         final Set<String> sentinels = StringUtils.commaDelimitedListToSet(sentinelCsv);
-        return new JedisSentinelConnectionFactory(masterName, sentinels);
+        if (isNotBlank(masterName) && !sentinels.isEmpty()) {
+            return new JedisSentinelConnectionFactory(masterName, sentinels);
+        }
+
+        final JedisConnectionFactory connectionFactory = new JedisConnectionFactory();
+        connectionFactory.setHostName(redisHost);
+        connectionFactory.setPort(redisPort);
+        return connectionFactory;
     }
 
     @Bean
